ff10c987ff9da2879c8613d27097e1a7a1c80849
Marcelo Azevedo <marcelobernardy@gmail.com>
2017-11-29 18:22:36 -0200

    Vers Final

--- src/pkg_genoma/App.java
+++ src/pkg_genoma/App.java
@@ -1,5 +1,101 @@
 package pkg_genoma;
+import javafx.*;
 
-public class App {
+import java.io.FileNotFoundException;
+import java.lang.System;
 
+
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	private ListView<Gene> lvGene;
+	private PasswordField pfPass;
+	
+	
+	@Override
+	public void start(Stage primaryStage)throws FileNotFoundException {
+		Genoma g = new Genoma();
+		GridPane grid = new GridPane();
+		//root.setAlignment(Pos.BASELINE_RIGHT);
+		grid.setPadding(new Insets(25,25,25,25));
+		grid.setVgap(200);
+		grid.setHgap(400);
+
+		// Painel do usuario/senha
+		Text scenetitle = new Text("Arquivo: sequence.txt");
+		scenetitle.setFont(Font.font("Tahoma",FontWeight.NORMAL, 16));
+		grid.add(scenetitle, 0, 0);
+		Label lbUser = new Label("Genes:");
+		grid.add(lbUser, 0, 1);
+		lvGene = new ListView<Gene>();
+		lvGene.setItems(g.getGenes());
+		grid.add(lvGene, 1, 1);
+		Label lbPass = new Label("Senha:");
+		grid.add(lbPass, 0, 2);
+		pfPass = new PasswordField();
+		grid.add(pfPass, 1, 2);
+
+		// Painel dos botoes
+		HBox hbBut = new HBox();
+		hbBut.setAlignment(Pos.BOTTOM_CENTER);
+		hbBut.setPadding(new Insets(10,10,10,10));
+		Button btOk = new Button("Ok");
+		Button btCancela = new Button("Cancela");
+		hbBut.getChildren().add(btOk);
+		hbBut.getChildren().add(btCancela);
+
+		//Painel geral
+		VBox root = new VBox();
+		root.setAlignment(Pos.CENTER);
+		root.getChildren().add(grid);
+		root.getChildren().add(hbBut);
+
+		/*btOk.setOnAction(e->{
+			if (lvGene.getText().equals("Zezinho")&&
+					pfPass.getText().equals("segredo")){
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Acesso liberado");
+				alert.setHeaderText(null);
+				alert.setContentText("Usuario e senha corretos !!");
+				alert.show();
+			}else{
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Oops!!");
+				alert.setHeaderText(null);
+				alert.setContentText("Usuario ou senha incorretos !!");           		
+				alert.show();
+			}
+		});*/
+
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Projeto GENOMA");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
+
+
--- src/pkg_genoma/Genoma.java
+++ src/pkg_genoma/Genoma.java
@@ -13,13 +13,17 @@ import java.util.regex.Pattern;
 import javax.sound.midi.Sequence;
 
 import com.sun.org.apache.bcel.internal.generic.LSTORE;
+import com.sun.scenario.effect.Merge;
 
 public class Genoma {
-    private List<Gene> genes;
-    AminoacidTable amino = new AminoacidTable();
+	List<Character> sequence = new ArrayList<>(200);
+	private List<Gene> genes;
+	//List<Character> sequence = new ArrayList<>(200);
+
+	AminoacidTable amino = new AminoacidTable();
 	private void loadData(File sourceFile) throws FileNotFoundException {
 		if (sourceFile == null) return;
-		
+
 		Pattern pattern = Pattern.compile("(\\[(.*?)\\])");
 		Pattern patloc = Pattern.compile("(\\[location=(\\d+)..(\\d+)\\])");
 		Pattern patLocus = Pattern.compile("(\\[locus_tag=(.*?)\\])");
@@ -29,7 +33,7 @@ public class Genoma {
 		String locus = "";
 		long begin = -1;
 		long end = -1;
-		List<Character> sequence = new ArrayList<>(200);
+		//List<Character> sequence = new ArrayList<>(200);
 		List <String> sequenceString = new ArrayList<>();
 		// Percorre o arquivo do genoma
 		while (data.hasNextLine()) {
@@ -38,10 +42,10 @@ public class Genoma {
 			if (line.length() > 0 && line.charAt(0) == '>') {
 				if (begin != -1) { // Se ja tem um Gene para montar, monta e armazena
 					// Cria gene e acrescenta na lista
-                    Gene gene = new Gene(locus,begin,end,sequence);
-                    genes.add(gene);
-                    
-                    // Limpa os campos para ler o prï¿½ximo gene ...
+					Gene gene = new Gene(locus,begin,end,sequence);
+					genes.add(gene);
+
+					// Limpa os campos para ler o prï¿½ximo gene ...
 					locus = "";
 					begin = -1;
 					end = -1;
@@ -64,174 +68,191 @@ public class Genoma {
 				}
 			} else { // Se ï¿½ uma linha de sequencia de gene ...
 				for (int i = 0; i < line.length(); i++) {
-					
+
 					sequence.add(line.charAt(i));
-					
+
 				}
 			}
-			
+
 		}
+
 		/*List <String>lst = compareAminoacid(mergeCharacter(sequence));
 		for(int i = 0;i<lst.size();i++) {
 			System.out.println(lst.get(i));
 		}
 		int c =compareCorrect(lst);	
 		System.out.println(c);*/
-		
-		List<String> sequenciaCorreta = compareMerge(sequence);
-		for(int i=0;i<sequenciaCorreta.size();i++){
-			//System.out.println(sequenciaCorreta.get(i));
-		}
+
+		List<String> sequenciaCorreta = compareMerge();
+		/*for(int i=0;i<sequenciaCorreta.size();i++){
+			System.out.println(sequenciaCorreta.get(i));
+		}*/
 		data.close();
 	}
-	public List mergeCharacter(List<Character> lst) {
-        List<String> t = new ArrayList<>();
-        String palavra = "";
-        ListIterator<Character> it = lst.listIterator();
+	public void imprimindoListas(){
+		List <String> merge = mergeCharacter();
+		List <String> mergePlusOne = mergeCharacterPlusOne();
+		List <String> mergePlusTwo = mergeCharacterPlusTwo();
+		List <String> mergeContrario = mergeCharacterContrario();
+		List <String> mergeContrarioPlusOne = mergeCharacterPlusOne();
+		List <String> mergeContrarioPlusTwo = mergeCharacterContrarioLessTwo();
 
-        while(it.hasNext()) {
-            palavra = palavra + it.next();
+		merge.forEach(p -> System.out.println("Ordem 5-3-1\n"+p));
+		mergePlusOne.forEach(p -> System.out.println("Ordem 5-3-2\n"+p));
+		mergePlusTwo.forEach(p -> System.out.println("Ordem 5-3-3\n"+p));
+		mergeContrario.forEach(p -> System.out.println("Ordem 3-5-1\n"+p));
+		mergeContrarioPlusOne.forEach(p -> System.out.println("Ordem 3-5-2\n"+p));
+		mergePlusTwo.forEach(p -> System.out.println("Ordem 3-5-3\n"+p));
 
-            if (palavra.length() % 3 == 0) {
-                t.add(palavra);
-                palavra = "";
-            }
-        }
-        return  t;
-    }
+	}
+	public List mergeCharacter() {
+		List<String> t = new ArrayList<>();
+		String palavra = "";
+		ListIterator<Character> it = sequence.listIterator();
 
-    public List mergeCharacterPlusOne(List<Character> lst) {
-        List<String> t = new ArrayList<>();
-        String palavra = "";
-        ListIterator<Character> it = lst.listIterator();
-        it.next();
+		while(it.hasNext()) {
+			palavra = palavra + it.next();
 
-        while(it.hasNext()) {
-            palavra = palavra + it.next();
+			if (palavra.length() % 3 == 0) {
+				t.add(palavra);
+				palavra = "";
+			}
+		}
+		return  t;
+	}
 
-            if (palavra.length() % 3 == 0) {
-                t.add(palavra);
-                palavra = "";
-            }
-        }
-        return  t;
+	public List mergeCharacterPlusOne() {
+		List<String> t = new ArrayList<>();
+		String palavra = "";
+		ListIterator<Character> it = sequence.listIterator();
+		it.next();
 
-    }
-    public List<String> compareMerge(List <Character>lst){
-    	
-    	int maior = 0;
-    	List<Integer> t = new ArrayList<>();
-    	
-    	t.add(compareCorrect(compareAminoacid(mergeCharacter(lst))));
-    	t.add(compareCorrect(compareAminoacid(mergeCharacterPlusOne(lst))));
-    	t.add(compareCorrect(compareAminoacid(mergeCharacterPlusTwo(lst))));
-    	t.add(compareCorrect(compareAminoacid(mergeCharacterContrario(lst))));
-    	t.add(compareCorrect(compareAminoacid(mergeCharacterContrarioLessOne(lst))));
-    	t.add(compareCorrect(compareAminoacid(mergeCharacterContrarioLessTwo(lst))));
-    	
-    	for(int i = 0; i<t.size();i++) {
-    		if(maior < t.get(i)) {
-    			maior = t.get(i);
-    		}
-    	}
-    	
-    	return enviarListaCorreta(maior,lst);
-    }
+		while(it.hasNext()) {
+			palavra = palavra + it.next();
 
-    private List<String> enviarListaCorreta(int maior, List<Character> lst) {
-		if(compareCorrect(compareAminoacid(mergeCharacter(lst)))==maior){
-			return compareAminoacid(mergeCharacter(lst));
-		}else if(compareCorrect(compareAminoacid(mergeCharacterPlusOne(lst))) == maior){
-			return compareAminoacid(mergeCharacterPlusOne(lst));
-		}else if(compareCorrect(compareAminoacid(mergeCharacterPlusTwo(lst))) == maior){
-			return compareAminoacid(mergeCharacterPlusTwo(lst));
-		}else if(compareCorrect(compareAminoacid(mergeCharacterContrario(lst)))==maior){
-			return compareAminoacid(mergeCharacterContrario(lst));
-		}else if(compareCorrect(compareAminoacid(mergeCharacterContrarioLessOne(lst)))==maior){
-			return compareAminoacid(mergeCharacterContrarioLessOne(lst));
-		}else if(compareCorrect(compareAminoacid(mergeCharacterContrarioLessTwo(lst)))==maior){
-			return compareAminoacid(mergeCharacterContrarioLessTwo(lst));
+			if (palavra.length() % 3 == 0) {
+				t.add(palavra);
+				palavra = "";
+			}
+		}
+		return  t;
+
+	}
+	public List<String> compareMerge(){
+
+		int maior = 0;
+		List<Integer> t = new ArrayList<>();
+
+		t.add(compareCorrect(compareAminoacid(mergeCharacter())));
+		t.add(compareCorrect(compareAminoacid(mergeCharacterPlusOne())));
+		t.add(compareCorrect(compareAminoacid(mergeCharacterPlusTwo())));
+		t.add(compareCorrect(compareAminoacid(mergeCharacterContrario())));
+		t.add(compareCorrect(compareAminoacid(mergeCharacterContrarioLessOne())));
+		t.add(compareCorrect(compareAminoacid(mergeCharacterContrarioLessTwo())));
+
+		for(int i = 0; i<t.size();i++) {
+			if(maior < t.get(i)) {
+				maior = t.get(i);
+			}
+		}
+
+		return enviarListaCorreta(maior,sequence);
+	}
+
+	private List<String> enviarListaCorreta(int maior, List<Character> lst) {
+		if(compareCorrect(compareAminoacid(mergeCharacter()))==maior){
+			return compareAminoacid(mergeCharacter());
+		}else if(compareCorrect(compareAminoacid(mergeCharacterPlusOne())) == maior){
+			return compareAminoacid(mergeCharacterPlusOne());
+		}else if(compareCorrect(compareAminoacid(mergeCharacterPlusTwo())) == maior){
+			return compareAminoacid(mergeCharacterPlusTwo());
+		}else if(compareCorrect(compareAminoacid(mergeCharacterContrario()))==maior){
+			return compareAminoacid(mergeCharacterContrario());
+		}else if(compareCorrect(compareAminoacid(mergeCharacterContrarioLessOne()))==maior){
+			return compareAminoacid(mergeCharacterContrarioLessOne());
+		}else if(compareCorrect(compareAminoacid(mergeCharacterContrarioLessTwo()))==maior){
+			return compareAminoacid(mergeCharacterContrarioLessTwo());
 		}
 		return null;
 	}
-	public List mergeCharacterPlusTwo(List<Character> lst) {
-        List<String> t = new ArrayList<>();
-        String palavra = "";
-        ListIterator<Character> it = lst.listIterator();
-        it.next();
-        it.next();
+	public List mergeCharacterPlusTwo() {
+		List<String> t = new ArrayList<>();
+		String palavra = "";
+		ListIterator<Character> it = sequence.listIterator();
+		it.next();
+		it.next();
 
-        while(it.hasNext()) {
-            palavra = palavra + it.next();
+		while(it.hasNext()) {
+			palavra = palavra + it.next();
 
-            if (palavra.length() % 3 == 0) {
-                t.add(palavra);
-                palavra = "";
-            }
-        }
-        return  t;
+			if (palavra.length() % 3 == 0) {
+				t.add(palavra);
+				palavra = "";
+			}
+		}
+		return  t;
 
-    }
-    public List mergeCharacterContrario(List<Character> lst) {
-        List<String> t = new ArrayList<>();
-        String palavra = "";
-        ListIterator<Character> it = lst.listIterator(lst.size());
+	}
+	public List mergeCharacterContrario() {
+		List<String> t = new ArrayList<>();
+		String palavra = "";
+		ListIterator<Character> it = sequence.listIterator(sequence.size());
 
-        while(it.hasPrevious()) {
-            palavra = palavra + it.previous();
+		while(it.hasPrevious()) {
+			palavra = palavra + it.previous();
 
-            if (palavra.length() % 3 == 0) {
-                t.add(palavra);
-                palavra = "";
-            }
-        }
-        return  t;
-    }
+			if (palavra.length() % 3 == 0) {
+				t.add(palavra);
+				palavra = "";
+			}
+		}
+		return  t;
+	}
 
-    public List mergeCharacterContrarioLessOne(List<Character> lst) {
-        List<String> t = new ArrayList<>();
-        String palavra = "";
-        ListIterator<Character> it = lst.listIterator(lst.size());
-        it.previous();
+	public List mergeCharacterContrarioLessOne() {
+		List<String> t = new ArrayList<>();
+		String palavra = "";
+		ListIterator<Character> it = sequence.listIterator(sequence.size());
+		it.previous();
 
-        while(it.hasPrevious()) {
-            palavra = palavra + it.previous();
+		while(it.hasPrevious()) {
+			palavra = palavra + it.previous();
 
-            if (palavra.length() % 3 == 0) {
-                t.add(palavra);
-                palavra = "";
-            }
-        }
-        return  t;
-    }
+			if (palavra.length() % 3 == 0) {
+				t.add(palavra);
+				palavra = "";
+			}
+		}
+		return  t;
+	}
 
-    public List mergeCharacterContrarioLessTwo(List<Character> lst) {
-        List<String> t = new ArrayList<>();
-        String palavra = "";
-        ListIterator<Character> it = lst.listIterator(lst.size());
-        it.previous();
-        it.previous();
+	public List mergeCharacterContrarioLessTwo() {
+		List<String> t = new ArrayList<>();
+		String palavra = "";
+		ListIterator<Character> it = sequence.listIterator(sequence.size());
+		it.previous();
+		it.previous();
 
-        while(it.hasPrevious()) {
-            palavra = palavra + it.previous();
+		while(it.hasPrevious()) {
+			palavra = palavra + it.previous();
 
-            if (palavra.length() % 3 == 0) {
-                t.add(palavra);
-                palavra = "";
-            }
-        }
-        return  t;
-    }
+			if (palavra.length() % 3 == 0) {
+				t.add(palavra);
+				palavra = "";
+			}
+		}
+		return  t;
+	}
 	public List compareAminoacid(List <String> codon) {
 		List <String> amin = new ArrayList<>(200);
 		for(int i = 0; i<codon.size();i++) {
 			amin.add(amino.getAminoacid(codon.get(i)));
 		}
-		
+
 		return amin;
 	}
 	public int compareCorrect(List<String> amin) {
-		
+
 		for(int i = 0;i<amin.size();i++) {
 			if(amin.get(i)== "Met") {
 				return contaDistancia(amin,i);
@@ -250,20 +271,98 @@ public class Genoma {
 		}
 		return count;
 	}
+
+
+	public Genoma() throws FileNotFoundException{
+		//List<Character> sequence = new ArrayList<>(200);
+		super();
+		genes = new LinkedList<>();
+		loadData(new File("sequence.txt"));
+	}
+
+	public List<Gene> getGenes(){
+		return genes;
+	}
+	public static void menu(int opcao) throws FileNotFoundException {
+		Genoma genoma = new Genoma();
+		List<Gene> gene = genoma.getGenes();
+		switch (opcao) {
+		case 1:
+			for(int i = 0;i<gene.size();i++){
+				System.out.println(gene.get(i).getLocus());
+			}
+
+			//Lista locus
+			break;
+		case 2:
+			for(int i = 0;i<gene.size();i++){
+				//System.out.println(gene.get(i).getLocus());
+				System.out.println("Inicio: "+gene.get(i).getBegin());
+				System.out.println("Final: "+gene.get(i).getEnd()+"\n");
+
+
+			}
+			//Posições de início e fim dos genes dentro dos genomas
+			break;
+		case 3:
+			for(int i = 0;i<gene.size();i++){
+				System.out.println(gene.get(i).getBases());
+			}
+			break;
+		case 4:
+			genoma.imprimindoListas();
+						
+
+			break;
+		case 5:
+			System.out.println("Ordem 5-3-1\n"+genoma.compareAminoacid(genoma.mergeCharacter()));
+			System.out.println("Ordem 5-3-2\n"+genoma.compareAminoacid(genoma.mergeCharacterPlusOne()));
+			System.out.println("Ordem 5-3-3\n"+genoma.compareAminoacid(genoma.mergeCharacterPlusTwo()));
+			System.out.println("Ordem 3-5-1\n"+genoma.compareAminoacid(genoma.mergeCharacterContrario()));
+			System.out.println("Ordem 3-5-2\n"+genoma.compareAminoacid(genoma.mergeCharacterContrarioLessOne()));
+			System.out.println("Ordem 3-5-3\n"+genoma.compareAminoacid(genoma.mergeCharacterContrarioLessTwo()));
+
+			break;
+		case 6:
+			System.out.println(genoma.compareMerge());
+
+			break;
+		default:
+			System.exit(0);
+			break;
+		}
+	}
+
+	public static void main(String args[]) throws FileNotFoundException{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("########################");
+		System.out.println("#                      #");
+		System.out.println("#    PROJETO GENOMA    #");
+		System.out.println("#                      #");
+		System.out.println("########################\n\n");
+
+		while (true) {
+			System.out.println("\n");
+			System.out.println("1 - Listar Locus");
+			System.out.println("2 - Posições de início e fim dos genes dentro dos genomas");
+			System.out.println("3 - Sequência de bases dos genes");
+			System.out.println("4 - Sequência de bases dos genes agrupadas em códons");
+			System.out.println("5 - Sequência de aminoácidos correspondente à sequência de códons indicada");
+			System.out.println("6 - Sequência “correta” de aminoácidos");
+			System.out.println("0 - Sair\n");
+
+			System.out.print("Selecione a opção desejada: ");
+			menu(in.nextInt());
+
+		
+		}
+	}
+
+
+
+
+
+
 	
-	
-    public Genoma() throws FileNotFoundException{
-    	//List<Character> sequence = new ArrayList<>(200);
-    	genes = new LinkedList<>();
-    	loadData(new File("sequence.txt"));
-    }
-    
-    public List<Gene> getGenes(){
-    	return genes;
-    }
-    
-    public static void main(String args[]) throws FileNotFoundException{
-        Genoma genoma = new Genoma();
-        System.out.println(genoma.toString());
-    }
 }
